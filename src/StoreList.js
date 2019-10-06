cc9867cbf63d385f325362786be4ab61a0a39247
kivancsahici@yahoo.com <kivancsahici@yahoo.com>
2019-10-06 22:53:30 +0200

    render store list based on static data

--- src/StoreList.js
+++ src/StoreList.js
@@ -3,14 +3,30 @@ import ListGroup from "react-bootstrap/ListGroup";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+const stores = [
+  {
+    addressName: 'Jumbo Eindhoven Boschdijk',
+    locationType: 'PuP'
+  },
+  {
+    addressName: 'Jumbo Eindhoven Geldropseweg',
+    locationType: 'PuP'
+  },
+  {
+    addressName: 'Jumbo Eindhoven Kastelenplein',
+    locationType: 'PuP'
+  }
+];
+
 function StoreList() {
   return (
     <div className="storeList">
       <ListGroup as="ul">
+      {stores.map((item,i) => (
         <ListGroup.Item as="li">
           <Row>
             <Col md={7} lg={7}>
-              <div className="addressName">Jumbo Eindhoven Boschdijk</div>
+              <div className="addressName">{item.addressName}</div>
             </Col>
             <Col md={4} lg={4}>
               <div className="bg-pup_supermarket"></div>
@@ -18,20 +34,10 @@ function StoreList() {
           </Row>
           <small>Currently open, closes at 22:00</small>
         </ListGroup.Item>
-        <ListGroup.Item as="li">
-          <Row>
-            <Col md={7} lg={7}>
-              <div className="addressName">Jumbo Eindhoven Geldropseweg</div>
-            </Col>
-            <Col md={4} lg={4}>
-              <div className="bg-pup_supermarket"></div>
-            </Col>
-          </Row>
-          <small>Currently open, closes at 22:00</small>
-        </ListGroup.Item>
-      </ListGroup>    
+      ))}
+      </ListGroup>
     </div>
-  );
+  )
 }
 
 export default StoreList;
