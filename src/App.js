940ae83dd0dfd2835e5df4e25071aa3e49ff6dbe
kivancsahici@yahoo.com <kivancsahici@yahoo.com>
2019-10-06 23:48:36 +0200

    wrap up the component for detailed search

--- src/App.js
+++ src/App.js
@@ -1,103 +1,38 @@
 import React from "react";
-
-import axios from 'axios';
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Cities from './Cities';
-import Streets from './Streets';
 import ClosestJumbo from './ClosestJumbo';
 import DetailedSearch from './DetailedSearch';
 import StoreList from './StoreList';
 import { Link, Switch, Route } from 'react-router-dom';
-
 import "./App.css";
 
-class App extends React.Component {
-  handleClick(e) {
-    axios.get(`http://localhost:8090/geoapi/v1/cities/`+e)
-     .then(res => {
-       const streets = res.data;
-       this.setState({ streets });
-     });
- }
-
-  constructor(props) {
-      super(props);
-      this.state = {
-        streets: [],
-        cities: []
-      };
-    }
-
-  renderStreets() {
-    return (
-      <Streets
-        value={this.state.streets}
-      />
-    );
-  }
-
-  renderCities() {
-    return (
-      <Cities
-        value={this.state.cities}
-        onChange={(e) => this.handleClick(e)}
-      />
-    );
-  }
-  componentDidMount() {
-
-    axios.get(`http://localhost:8090/geoapi/v1/cities`)
-      .then(res => {
-        const cities = res.data;
-        setTimeout(() => this.setState({ cities }), 1000);
-
-      });
-  }
-
-  render() {
-    return (
-      <div id="root-container">
-      <Navbar bg="light" expand="lg"></Navbar>
-      <Container fluid="true">
-        <Row className="justify-content-md-center">
-          <Col md={12} lg={4}>
-            <Row>
-              <Col md={12} lg={5}>
-              <Button variant="primary" block><Link to="/closestJumbo">Closest Jumbo</Link></Button>
-              <Button variant="primary" block><Link to="/detailedSearch">Detailed Search</Link></Button>
-              </Col>
-              <Col md={12} lg={7}>
-                <Form>
-                  {this.renderCities()}
-                  {this.renderStreets()}
-                  <Form.Group as={Col}>
-                  <Button variant="primary" type="submit">
-                    Search
-                  </Button>
-                  </Form.Group>
-                </Form>
-              </Col>
-            </Row>
-          </Col>
-          <Col md={12} lg={4} className="mapHolder">
-            <Switch>
-              <Route exact path="/closestJumbo" component={ClosestJumbo} />
-              <Route exact path="/detailedSearch/" component={DetailedSearch} />
-            </Switch>
-          </Col>
-          <Col md={12} lg={3} className="storeList">
-            <StoreList/>
-          </Col>
-        </Row>
-      </Container>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div id="root-container">
+    <Navbar bg="light" expand="lg"></Navbar>
+    <Container fluid="true">
+      <Row className="justify-content-md-center">
+        <Col md={12} lg={2}>
+          <Button variant="primary" block><Link to="/closestJumbo">Closest Jumbo</Link></Button>
+          <Button variant="primary" block><Link to="/detailedSearch">Detailed Search</Link></Button>
+        </Col>
+        <Col md={12} lg={4} className="mapHolder">
+          <Switch>
+            <Route exact path="/closestJumbo" component={ClosestJumbo} />
+            <Route exact path="/detailedSearch/" component={DetailedSearch} />
+          </Switch>
+        </Col>
+        <Col md={12} lg={3} className="storeList">
+          <StoreList/>
+        </Col>
+      </Row>
+    </Container>
+    </div>
+  );
 }
 
 export default App;
--- src/DetailedSearch.js
+++ src/DetailedSearch.js
@@ -1,11 +1,67 @@
 import React from 'react';
+import Cities from './Cities';
+import Streets from './Streets';
+import axios from 'axios';
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
 
-function DetailedSearch() {
-  return (
-    <div className="DetailedSearch">
-      <h1>Detailed Search</h1>
-    </div>
-  );
+class DetailedSearch extends React.Component {
+  handleClick(e) {
+    axios.get(`http://localhost:8090/geoapi/v1/cities/`+e)
+     .then(res => {
+       const streets = res.data;
+       this.setState({ streets });
+     });
+ }
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        streets: [],
+        cities: []
+      };
+    }
+
+  renderStreets() {
+    return (
+      <Streets
+        value={this.state.streets}
+      />
+    );
+  }
+
+  renderCities() {
+    return (
+      <Cities
+        value={this.state.cities}
+        onChange={(e) => this.handleClick(e)}
+      />
+    );
+  }
+  componentDidMount() {
+
+    axios.get(`http://localhost:8090/geoapi/v1/cities`)
+      .then(res => {
+        const cities = res.data;
+        setTimeout(() => this.setState({ cities }), 1000);
+
+      });
+  }
+
+  render() {
+    return (
+      <div className="DetailedSearch">
+        <Form>
+          {this.renderCities()}
+          {this.renderStreets()}
+          <Form.Group as={Col}>
+            <Button variant="primary" type="submit">Search</Button>
+          </Form.Group>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default DetailedSearch;
